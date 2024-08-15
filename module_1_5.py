8eb41611481f1eb1628fbd9257ab45212e53275e
Kastrumstix <ma_stix@mail.ru>
2024-08-15 15:31:07 +0300

    Add files via upload

---
+++ module_1_5.py
@@ -0,0 +1,7 @@
+immutable_var = 1, 2, 3, "String", True
+print(immutable_var)
+# immutable_var[0] = 3
+mutable_list = [1, 2, 'a', 'b', 'Str', True]
+mutable_list[0] = 3
+mutable_list[3] = 5
+print(mutable_list)
