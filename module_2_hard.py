e559500b5612b1c8d515fb71f8d45205fddc25af
Kastrumstix <ma_stix@mail.ru>
2024-08-24 20:53:45 +0300

    Add files via upload

--- module_2_hard.py
+++ module_2_hard.py
@@ -1,13 +1,15 @@
-#g = int(input("ведите число от 3 до 20: ",))
+#import random
+g = int(input("ведите число от 3 до 20: ",))
 mass = []
-import random
-g = random.randint(3, 20)
-print(g)
+string = ''
+#g = random.randint(3, 20)
 for i in range(1, g+1):
-    for j in range(1, i+1):
-        if (i+j) == g or ( g % (i+j) == 0 and j != 0):
-            mass.append(j)
+    for j in range(i+1, g+1):
+        if (i+j) == g or g % (i+j) == 0:
             mass.append(i)
-
-
-print(mass)
+            mass.append(j)
+        else:
+            continue
+for el in range(len(mass)):
+    string += str(mass[el])
+print(string)
